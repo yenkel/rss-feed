5ce69db32b7a710a6ecda34f1f7ace9ddf04c308
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-05-23 00:53:54 +0300

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,33 @@
-const path = require('path');
+var webpack = require("webpack");
+var path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './client/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+//     template: './client/index.html',
+//     filename: 'index.html',
+//     inject: 'body'
+// })
 
 module.exports = {
-    entry: './client/index.js',
+    entry: {
+        app: "./src/App.js"
+    },
     output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js'
+        filename: "build/bundle.js",
+        sourceMapFilename: "build/bundle.map"
     },
+    devtool: '#source-map',
+    // plugins: [
+    //     new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    // ],   
     module: {
-        loaders: [
-            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
-    },
-    plugins: [HtmlWebpackPluginConfig]
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    }
 }
