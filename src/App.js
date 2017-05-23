c690e06a05685d8d718aa233bca8dcfc32f17a5e
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-05-23 12:52:54 +0300

    chat component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Chat from './components/Chat';
 
 class App extends Component {
     render() {
         return (
             <div>
                <h1>Welcome to Twitch chat!!</h1>
+               <Chat />
             </div>
         );
     }
---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+    render() {
+        return (
+
+            <div>This is the Chat Component</div>
+
+        )
+    }
+}
+
+export default Chat;
