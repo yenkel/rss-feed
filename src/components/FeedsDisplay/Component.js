f6dbe97a2b553b8735df69e6d66472d48ec3cc74
yenkel <yenkel.mayost@gmail.com>
2018-02-05 13:28:19 +0200

    display feeds of specific url

---
+++ src/components/FeedsDisplay/Component.js
@@ -0,0 +1,38 @@
+import React from 'react'
+// Style
+import styles from './Style.scss'
+import cx from "classnames"
+import Button from '../Button'
+import moment from 'moment'
+
+const FeedsDisplay = React.createClass({
+  renderFeeds(feeds) {
+    if (feeds && feeds.length) {
+       return (
+         feeds.map((feed, index) => {
+           return (
+               <div key={index} className={styles.feedContainer}>
+                   <div className={styles.feedTitle}>
+                     {feed.title} - {moment(feed.pubDate, 'YYYY-MM-DD').format('DD/MM/YYYY')}
+                   </div>
+                   <div className={styles.feedContent}>
+                     {feed.content.replace(/(<([^>]+)>)/ig," ")}
+                   </div>
+                </div>
+            )
+         })
+       )
+    }
+  },
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className={styles.url}>{this.props.url}</div>
+        {this.renderFeeds(this.props.feeds)}
+      </div>
+    )
+  }
+
+})
+
+export default FeedsDisplay
--- src/components/RssContainer/Component.js
+++ src/components/RssContainer/Component.js
@@ -4,23 +4,62 @@ import axios from 'axios'
 import styles from './Style.scss'
 // Components
 import Pane from '../Pane'
+import FeedsDisplay from '../FeedsDisplay'
 
 class RssContainer extends React.Component {
   constructor(props) {
     super(props)
+    this.lookForFeeds = this.lookForFeeds.bind(this)
+    this.changeUrl = this.changeUrl.bind(this)
+    this.state = {
+      feeds: [],
+      inputUrl: '',
+      url: 'http://www.feedforall.com/sample.xml'
+    }
   }
   componentDidMount() {
-      axios.get('https://api.rss2json.com/v1/api.json?rss_url=http://www.feedforall.com/sample.xml')
-        .then(res => {
-          console.log(res)
-          // const books = res.data.items.map(obj => obj.volumeInfo)
-          // this.setState({ books })
-      })
+      this.lookForFeeds()
+  }
+  lookForFeeds() {
+    axios.get('https://api.rss2json.com/v1/api.json?rss_url=' + this.state.url)
+      .then(res => {
+        const feeds = res.data.items.map(obj => obj)
+        this.setState({ feeds })
+    })
+  }
+  changeUrl(e) {
+    const newState = this.state
+    newState.inputUrl = e.target.value
+    this.setState({ newState })
   }
   render() {
+    const { feeds, url } = this.state
+    console.log(feeds)
     return (
       <div className={styles.root}>
-        <Pane title={'Books Library'} className={styles.booksPaneContainer}>
+        <Pane title={"URL Selector"} className={styles.sidebarContainer}
+          classNameContainer={styles.paneContainer}>
+          <div className={styles.urlContainer}>
+            <div className={styles.header}>
+              <div className={styles.inputContainer}>
+                <input className={styles.input}
+                       placeholder='Input URL'
+                       value={url}
+                       onChange={this.changeUrl}>
+               </input>
+              </div>
+              <div className={styles.imageContainer}>
+                <img src={"https://png.icons8.com/color/540/search.png"}
+                    onClick={() => this.setState({ url: inputUrl})}/>
+              </div>
+            </div>
+          </div>
+        </Pane>
+        <Pane title={"Feeds Display"} className={styles.mainScreenContainer}
+          classNameContainer={styles.paneContainer}>
+          <div className={styles.feedsContainer}>
+             <FeedsDisplay url={url} feeds={feeds}/>
+          </div>
         </Pane>
       </div>
     )
